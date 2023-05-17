30b307ece99c71c45898a0c7cb0c18a24396d66b
sudocurse <sudocurse@gmail.com>
2023-05-16 21:53:41 -0700

    UI for adding blocks

--- script.js
+++ script.js
@@ -18,6 +18,36 @@ class Block {
     }
 }
 
+class AddBlock extends Block {
+    constructor() {
+        super();
+        this.data = "Add Block +";
+        this.func = function() {
+            let input = document.getElementsByClassName("add-block-input")[0];
+            let link = input.value;
+            if (!link) {
+                return;
+            }
+            console.log("adding " + link);
+
+        }
+    }
+
+    draw() {
+        let element = super.draw();
+        // prepend text input field
+        element.setAttribute("class", "block add-block");
+        let input = document.createElement("input");
+        input.setAttribute("class", "add-block-input");
+        input.setAttribute("type", "text");
+        input.setAttribute("placeholder", "Enter URL");
+        element.prepend(document.createElement("br"));
+        element.prepend(input);
+
+        return element;
+    }
+}
+
 function makeBlock(link, position=-1) {
     // make the block object out of a link
     let block = new Block(link);
@@ -26,12 +56,22 @@ function makeBlock(link, position=-1) {
     return block.draw();
 }
 
+function makeAddBlock() {
+    let block = new AddBlock();
+    return block.draw();
+}
+
 window.onload = function run() {
     let rootDiv = document.getElementById("root");
     rootDiv.textContent = "My Collection";
 
     let blocks = [];
-    let b = makeBlock("Add New Block"); // todo: add some functionality
+
+    let shelf = document.createElement("div");
+    shelf.setAttribute("id", "shelf");
+    rootDiv.append(shelf);
+
+    let b = makeAddBlock();
     blocks.push(b);
 
     b = makeBlock("https://duckduckgo.com/");
@@ -42,10 +82,6 @@ window.onload = function run() {
     blocks.push(makeBlock("ipns://docs.ipfs.tech/"));
     blocks.push(makeBlock("ipns://libp2p.io/"));
     blocks.push(makeBlock("ipfs://bafybeiggiypjr5l5xwjtgbc5hndu3xigcl6arf62mlx5ekhflemjhhifpy"));
-
-    let shelf = document.createElement("div");
-    shelf.setAttribute("id", "shelf");
-    rootDiv.append(shelf);
 
     for (let block of blocks) {
         shelf.append(block);
